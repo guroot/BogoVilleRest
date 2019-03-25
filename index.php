a2f45daf1b98e7907a2be377959648874280af70
fletch <jfleury@collegeshawinigan.qc.ca>
2019-03-24 21:38:10 -0400

    Implémentation du framework SLIM Consultation des régions /region/{id}

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jonathan Fleury
+ * Date: 19-03-24
+ * Time: 20:16
+ */
+
+// Permet de charger automatiquement les librairies du framework Slim
+require 'vendor/autoload.php';
+
+// instantiate the App object
+$app = new \Slim\App();
+
+// Add route callbacks
+$app->get('/', function ($request, $response, $args) {
+    return $response->withStatus(200)->write('Ceci est un service Rest et ne devrait pas être accédé directement');
+});
+
+
+$pdo =  new PDO('mysql:host=localhost;dbname=bogoville', 'root', '');
+
+
+//********************************************************************************
+//Régions
+//********************************************************************************
+
+$app->get('/region/{id}', function ($request, $response, $args) use($pdo){
+    $regionModel = new \model\Region($pdo);
+    $data = $regionModel->getById($args['id']);
+    if($data)
+        $response = $response->withJson($regionModel->getById($args['id']));
+    else
+        return $response->withStatus(404)
+            ->withHeader('Content-Type', 'application/json;charset=utf-8')
+            ->write('Enregistrement introuvable');
+    return $response;
+});
+
+
+
+
+// Run application
+$app->run();
+

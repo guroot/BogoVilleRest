ebf9855eb6ce43bd62846cb0b2ac01b69f49abaa
fletch <jfleury@collegeshawinigan.qc.ca>
2019-04-05 09:53:36 -0400

    Commit

---
+++ model/Region.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace model;
+/**
+ * Created by PhpStorm.
+ * User: fletch
+ * Date: 19-03-24
+ * Time: 20:38
+ */
+class Region
+{
+
+    /**
+     * @var PDO
+     */
+    private $_pdo;
+
+
+    /**
+     * Region constructor.
+     */
+    public function __construct($pdo)
+    {
+        $this->_pdo = $pdo;
+    }
+
+
+    // Retourne l'enregistrement à partir
+    // de sa clé primaire auto-increment
+    public function getById($id)
+    {
+        $statement = $this->_pdo->prepare("SELECT * FROM region WHERE idregion=?");
+        $statement->execute([$id]);
+        return $statement->fetchObject();
+    }
+
+}

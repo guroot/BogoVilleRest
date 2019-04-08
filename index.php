139007c0af3d7203636941ea297dd0a6066bb48b
gaby32 <39409503+gaby32@users.noreply.github.com>
2019-04-08 14:42:20 -0400

    region

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Jonathan Fleury
  * Date: 19-03-24
  * Time: 20:16
- */ 
+ */
 
 // Permet de charger automatiquement les librairies du framework Slim
 require 'vendor/autoload.php';
 
+$config = ['settings' => ['displayErrorDetails' => true, 'determineRouteBeforeAppMiddleware' => true,'addContentLengthHeader' => false]];
+
 // instantiate the App object
-$app = new \Slim\App();
+$app = new \Slim\App($config);
+
 
 // Add route callbacks
 $app->get('/', function ($request, $response, $args) {
@@ -18,18 +22,17 @@ $app->get('/', function ($request, $response, $args) {
 });
 
 
-$pdo =  new PDO('mysql:host=localhost;dbname=bogoville', 'root', '');
+$pdo = new PDO('mysql:host=localhost;dbname=bogoville', 'root', '');
 
-//POULE
 
 //********************************************************************************
 //Régions
 //********************************************************************************
 
-$app->get('/region/{id}', function ($request, $response, $args) use($pdo){
+$app->get('/region/{id}', function ($request, $response, $args) use ($pdo) {
     $regionModel = new \model\Region($pdo);
     $data = $regionModel->getById($args['id']);
-    if($data)
+    if ($data)
         $response = $response->withJson($regionModel->getById($args['id']));
     else
         return $response->withStatus(404)
@@ -38,8 +41,37 @@ $app->get('/region/{id}', function ($request, $response, $args) use($pdo){
     return $response;
 });
 
+$app->post('/region', function ($request, $response, $args) use ($pdo) {
+    $regionModel = new \model\Region($pdo);
+    $nom =  $request->getParam('nom');
 
+    $response->withJson($regionModel->insert($nom));
 
+    return $response;
+});
+
+$app->delete('/region/{id}', function ($request, $response, $args) use ($pdo) {
+    $regionModel = new \model\Region($pdo);
+    $data = $regionModel->deleteById($args['id']);
+    if (!$data)
+        return $response->withStatus(404)
+            ->withHeader('Content-Type', 'application/json;charset=utf-8')
+            ->write('Enregistrement introuvable');
+    return $response;
+});
+
+$app->put('/type/{id}', function ($request, $response, $args) use($pdo){
+    $regionModel = new \model\Region($pdo);
+    $dataArray = $request->getParams();
+    $data = $regionModel->update($args['id'],$dataArray);
+    if($data)
+        $response = $response->withJson($regionModel->update($args['id'],$dataArray));
+    else
+        return $response->withStatus(404)
+            ->withHeader('Content-Type', 'application/json;charset=utf-8')
+            ->write('Impossible dupdater');
+    return $response;
+});
 
 // Run application
 $app->run();
--- model/Region.php
+++ model/Region.php
@@ -11,7 +11,7 @@ class Region
 {
 
     /**
-     * @var PDO
+     * @var \PDO
      */
     private $_pdo;
 
@@ -25,13 +25,44 @@ class Region
     }
 
 
+
     // Retourne l'enregistrement à partir
     // de sa clé primaire auto-increment
     public function getById($id)
     {
-        $statement = $this->_pdo->prepare("SELECT * FROM region WHERE idregion=?");
+        $statement = $this->_pdo->prepare("SELECT * FROM ". Region_Table::$TABLE_NAME ." WHERE ". Region_Table::$ID ." = ?");
         $statement->execute([$id]);
         return $statement->fetchObject();
     }
 
+    public function insert($nom)
+    {
+        $statement = $this->_pdo->prepare("INSERT INTO ". Region_Table::$TABLE_NAME ."(nom) ". " VALUES (?)");
+        return $statement->execute([$nom]);
+
+
+    }
+
+    public function deleteById($id)
+    {
+        $statement = $this->_pdo->prepare("DELETE FROM ". Region_Table::$TABLE_NAME ." WHERE ". Region_Table::$ID ." = ?");
+      return  $statement->execute([$id]);
+
+    }
+
+    public function update($id, $dataArray){
+
+        unset($dataArray["path"]);
+        $query = "UPDATE ". Region_Table::$TABLE_NAME . " SET ";
+        foreach($dataArray as $keys => $value)
+            if(array_key_last($dataArray) === $keys){
+                $query .= $keys . " = :" . $keys;
+            } else{
+                $query .= $keys . " = :" . $keys . ", ";
+            }
+        $query .= " WHERE " . Region_Table::$ID . " = :" . Region_Table::$ID;
+
+        $request = $this -> _pdo -> prepare($query);
+        return $request->execute(array_merge($dataArray, array(Region_Table::$ID => $id)));
+    }
 }

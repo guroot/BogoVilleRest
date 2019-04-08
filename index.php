89fe912e3b6f4e45e26559221447bf01099f3fbc
Cedrick <cedrick.guillet@gmail.com>
2019-04-08 14:43:37 -0400

    evenement

--- index.php
+++ index.php
@@ -4,43 +4,124 @@
  * User: Jonathan Fleury
  * Date: 19-03-24
  * Time: 20:16
- */ 
+ */
 
 // Permet de charger automatiquement les librairies du framework Slim
 require 'vendor/autoload.php';
 
+$isModelLegit = false;
+
+$configuration = [
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+];
+$c = new \Slim\Container($configuration);
+
 // instantiate the App object
-$app = new \Slim\App();
+$app = new \Slim\App($c);
 
 // Add route callbacks
 $app->get('/', function ($request, $response, $args) {
     return $response->withStatus(200)->write('Ceci est un service Rest et ne devrait pas être accédé directement');
 });
 
-
 $pdo =  new PDO('mysql:host=localhost;dbname=bogoville', 'root', '');
 
-//POULE
-
-//********************************************************************************
-//Régions
-//********************************************************************************
-
-$app->get('/region/{id}', function ($request, $response, $args) use($pdo){
-    $regionModel = new \model\Region($pdo);
-    $data = $regionModel->getById($args['id']);
-    if($data)
-        $response = $response->withJson($regionModel->getById($args['id']));
-    else
-        return $response->withStatus(404)
-            ->withHeader('Content-Type', 'application/json;charset=utf-8')
-            ->write('Enregistrement introuvable');
-    return $response;
+/*$app->any("\{model}\*", function ($request, $response, $args) use($pdo, $isModelLegit){
+    $test = false;
+    $modelName = $args['model'];
+    ucfirst($modelName);
+    $index = 1;
+    while(!$test){
+        if(substr($modelName, $index, 1) == "_" && $index !== strlen($modelName)-1 ){
+            substr_replace($modelName, strtoupper(substr($modelName, $index+1, 1)), $index+1, 1);
+        }
+        if($index == strlen($modelName)-1){
+            $test = true;
+            $modelName = str_replace("_", '', $modelName);
+        }
+        $index++;
+    }
+    if(is_subclass_of($args['model'], get_class(\model\DataAccess::class))){
+        global $isModelLegit;
+        $isModelLegit= true;
+    };
 });
 
+if($isModelLegit) {*/
 
+    $app->get("/{model}/{id}", function ($request, $response, $args) use ($pdo) {
+        $className =  "\model\\".ucfirst($args['model']);
+        $myGenericModel = new $className($pdo);
+        $data = $myGenericModel->getOneShitById($args['id']);
+        if ($data)
+            $response = $response->withJson($myGenericModel->getOneShitById($args['id']));
+        else
+            return $response->withStatus(404)
+                ->withHeader('Content-Type', 'application/json;charset=utf-8')
+                ->write('Enregistrement introuvable');
+        return $response;
+    });
+
+    $app->get("/{model}", function ($request, $response, $args) use ($pdo) {
+        $className =  "\model\\".ucfirst($args['model']);
+        $myGenericModel = new $className($pdo);
+        $data = $myGenericModel->getAllTheShit();
+        if ($data)
+            $response = $response->withJson($myGenericModel->getAllTheShit());
+        else
+            return $response->withStatus(404)
+                ->withHeader('Content-Type', 'application/json;charset=utf-8')
+                ->write('Enregistrement introuvable');
+        return $response;
+    });
+
+    $app->post("/{model}", function ($request, $response, $args) use ($pdo) {
+        $className = "\model\\".ucfirst($args['model']);
+        $myGenericModel = new $className($pdo);
+        $data = $request->getParsedBody();
+        if($data)
+            $response = $response->withJson($myGenericModel->insertTheShit($request->getParams(), $data));
+        else
+            return $response->withStatus(500)
+                ->withHeader('Content-Type', 'application/json;charset=utf-8')
+                ->write('Cannot insert the shit');
+    });
+
+    $app->put("/{model}/{id}", function ($request, $response, $args) use ($pdo) {
+        $className = "\model\\".ucfirst($args['model']);
+        $myGenericModel = new $className($pdo);
+        $data = $request->getParsedBody();
+        var_dump($data);
+        if($data)
+            $response = $myGenericModel->updateTheShit($args['id'], $request->getParams());
+        else
+            return $response->withStatus(406)
+                ->withHeader('Content-Type', 'application/json;charset=utf-8')
+                ->write('Cannot update the shit');
+    });
+
+    $app->delete("/{model}/{id}", function ($request, $response, $args) use ($pdo) {
+        $className = "\model\\".ucfirst($args['model']);
+        $myGenericModel = new $className($pdo);
+        $data = $myGenericModel->getOneShitById($args['id']);
+        var_dump($data);
+        if ($data)
+            $response = $response->withJson($myGenericModel->deleteOneShitById($args['id']));
+        else
+            return $response->withStatus(404)
+                ->withHeader('Content-Type', 'application/json;charset=utf-8')
+                ->write('Enregistrement introuvable');
+        return $response;
+    });
+
+/*} else {
+    $app->get('/*', function ($request, $response, $args) {
+        return $response->withStatus(201)->write('Roses are red, Violets are blue <br> This is not a place for someone like you');
+    });}*/
 
 
 // Run application
-$app->run();
+    $app->run();
 
--- model/Region.php
+++ model/Region.php
@@ -1,37 +1,25 @@
 <?php
-
-namespace model;
+namespace  model;
 /**
  * Created by PhpStorm.
  * User: fletch
  * Date: 19-03-24
  * Time: 20:38
  */
-class Region
-{
-
-    /**
-     * @var PDO
-     */
-    private $_pdo;
-
+class Region extends DataAccess {
 
     /**
      * Region constructor.
      */
     public function __construct($pdo)
     {
-        $this->_pdo = $pdo;
+        parent::__construct($pdo);
+        $this->_tableName = RegionTable::TABLE_NAME;
+        $this->_idColumnName = RegionTable::COLUMNS['ID'];
     }
 
-
-    // Retourne l'enregistrement à partir
-    // de sa clé primaire auto-increment
-    public function getById($id)
-    {
-        $statement = $this->_pdo->prepare("SELECT * FROM region WHERE idregion=?");
-        $statement->execute([$id]);
-        return $statement->fetchObject();
+    public function getAllColumns(){
+        return RegionTable::getAllColumns();
     }
 
 }
